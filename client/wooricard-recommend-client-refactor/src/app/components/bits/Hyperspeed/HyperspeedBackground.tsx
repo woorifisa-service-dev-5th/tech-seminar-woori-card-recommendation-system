91f56399eca6b6195caf81fdb742cfecb1f69b54
Jhcki222 <iqbox2@naver.com>
2025-09-22 16:46:58 +0900

    [BUILD] ec2 eslint build settings fix

--- client/wooricard-recommend-client-refactor/src/app/components/bits/Hyperspeed/HyperspeedBackground.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import Hyperspeed from '@/components/bits/Hyperspeed/Hyperspeed';
-import { hyperspeedPresets } from '@/components/bits/Hyperspeed/presets';
-
-export default function HyperspeedBackground() {
-    return <Hyperspeed effectOptions={hyperspeedPresets.future} />;
-}
